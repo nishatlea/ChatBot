e8e7dbebf7a9aca81e0841e936f90275e3b700d2
nishatlea <nishataurora1956@gmail.com>
2024-04-17 19:37:48 +0600

    till embeddings

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+import os
+from dotenv import load_dotenv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
 
+from langchain_community.document_loaders import PyPDFLoader
+from pinecone import ServerlessSpec
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+file_path = "data/books/alice_in_wonderland.pdf"
+loader = PyPDFLoader(file_path)
+documents = loader.load()
 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+docs = text_splitter.split_documents(documents)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from langchain_openai import OpenAIEmbeddings
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+model_name = 'text-embedding-ada-002'
+embeddings = OpenAIEmbeddings(
+    model=model_name,
+    openai_api_key=openai_api_key
+)
+
+from langchain_pinecone import Pinecone
+
+environment = 'us-east-1'
+pinecone_api_key = os.getenv('PINECONE_API_KEY')
+pinecone = Pinecone(api_key=pinecone_api_key)
+# indexes = pinecone.list_indexes()
+# print(indexes.names())
+pinecone.create_index(
+    name="serverless-index",
+    dimension=1536,
+    metric="cosine",
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+)
+
+pc = Pinecone(
+    pinecone_api_key=os.getenv('PINECONE_API_KEY'),
+    index_name='my_index',
+    embedding=embeddings
+)
+
+index = Pinecone.from_documents(docs, embeddings, index_name='my_index')
+retriever = index.as_retriever(search_type='similarity', search_kwargs={"k": 2})
+
